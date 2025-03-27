96d8256e08e2bae6114c8384fee03928eb5a1a47
hzh1225 <hzhhuang1225@gmail.com>
2025-03-27 20:50:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1; }
     slides[slideIndex - 1].style.opacity = 1;
-    setTimeout(showSlides, 4000);
+    setTimeout(showSlides, 2500);
 }
 
 document.addEventListener("DOMContentLoaded", showSlides);
